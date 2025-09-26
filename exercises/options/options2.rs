e43642e3c99fd89ffe75c7f4d21a9513a85e0d52
Your Name <you@example.com>
2025-09-26 12:14:21 +0800

    - 1. 修复了 simple_option 测试中的错误语法，将其改为正确的 if let 语句：
    ```
    if let Some(word) = 
    optional_target {
        assert_eq!(word, target);
    }
    ```
    这展示了Rust中 if let 语法的用法，用于优雅地处理Option类型的值。
    - 2.
    修复了 layered_option 测试中的错误语法，将其改为正确的 while let 语句：
    
    ```
    while let Some(Some(integer)) = 
    optional_integers.pop() {
        assert_eq!(integer, cursor);
        cursor -= 1;
    }
    ```
    这展示了Rust中嵌套Option类型的处理方式，可以在 while let 中堆叠模式匹配，同时处理vector.pop()返回的Option和向量中存储的Option值。
    - 3.
    添加了简单的 main 函数，使程序可以正常运行
    - 4.
    移除了"// I AM NOT DONE"标记

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -12,8 +10,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        // Make this an if let statement whose value is "Some" type
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,14 +27,17 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
+        // Make this a while let statement - stack Option<T>s to handle vector.pop()
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
 
         assert_eq!(cursor, 0);
     }
+}
+
+fn main() {
+    // Just a simple main function to make the program runnable
+    println!("options2.rs练习 - Option类型的if let和while let语法练习");
 }
